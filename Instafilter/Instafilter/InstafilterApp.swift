4b7639bdb1241576ef02a3d26ab57f2fdd19816c
Ponyo <ponyoshuiii@Gmail.com>
2022-08-10 14:51:31 +0800

    新项目，桥接SwiftUI与UIkit
    使用Core Image、onChange、读取用户照片库。

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Rich Bobo on 2022/7/19.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
