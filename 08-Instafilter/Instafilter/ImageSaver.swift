e262caab95e228172a3689563be2b4cab198b8e9
Ponyo <ponyoshuiii@Gmail.com>
2022-08-11 15:41:47 +0800

    新增功能
    1、选择不同效果
    2、保存图片

--- 08-Instafilter/Instafilter/ImageSaver.swift
+++ 08-Instafilter/Instafilter/ImageSaver.swift
@@ -5,15 +5,22 @@
 //  Created by Rich Bobo on 2022/8/9.
 //
 
-import SwiftUI
+import UIKit
 
 class ImageSaver: NSObject {
+   var successHandler: (() -> Void)?
+   var errorHandler: ((Error) -> Void)?
+   
    func writeToPhotoAlbum(image: UIImage) {
       UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
    }
 
       @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-         print("Save Finshed!")
+         if let error = error {
+            errorHandler?(error)
+         }else {
+            successHandler?()
+         }
       }
    }
 
